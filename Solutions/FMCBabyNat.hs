8c6b3d14e3dc177c5bd67c136128183b881a1017
Joamerson Islan <joamersonislan38@gmail.com>
2025-09-25 10:59:40 -0300

    feat: add definition for `pred`

--- Solutions/FMCBabyNat.hs
+++ Solutions/FMCBabyNat.hs
@@ -40,7 +40,8 @@ isZero (S _) = O
 
 -- pred is the predecessor but we define zero's to be zero
 pred :: Nat -> Nat
-pred = undefined
+pred O = O
+pred (S x) = x
 
 -- Output: O means False, S O means True
 even :: Nat -> Nat
