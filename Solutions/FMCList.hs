26fa4b536508f8b000a21512fdcc8a26c483ad61
Joamerson Islan <joamersonislan38@gmail.com>
2025-09-30 15:05:30 -0300

    feat: implement `drop`

--- Solutions/FMCList.hs
+++ Solutions/FMCList.hs
@@ -130,6 +130,10 @@ take 0 _ = []
 take n (x : xs) = x : take (n-1) xs
 
 -- drop
+drop :: Int -> [a] -> [a]
+drop _ [] = []
+drop 0 xs = xs
+drop n (_ : xs) = drop (n-1) xs
 
 -- takeWhile
 -- dropWhile
